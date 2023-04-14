d2137aea22e84f957e4a89a20a257be62ba5c916
Matt Hill <null>
2023-04-14 19:32:59 +0000

    WIP

--- src/main/java/org.redhat.janus/ExampleResource.java
+++ src/main/java/org.redhat.janus/ExampleResource.java
@@ -1,9 +1,12 @@
 package org.redhat.janus;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Set;
 
 @Path("/fruits")
 public class FruitResource {
